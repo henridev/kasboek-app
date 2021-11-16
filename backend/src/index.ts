653fa775b56e270660bf0c7abb7f33d806a8fba8
Henri De Bel <63241061+henridev@users.noreply.github.com>
2021-11-16 07:00:48 +0100

    add cert

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,21 @@
 import app from './app'
 import {createServer} from 'http'
+import {readFileSync} from 'fs'
+import {createServer as createSecureServer} from 'https'
 import {config} from 'dotenv'
 import path from "path";
 
 config({ path: path.join(__dirname, "../.env") });
 
-
-
+const options = {
+  key:readFileSync('../ssl/domain.key'),
+  cert:readFileSync('../ssl/domain.cert')
+};
 
 const server = createServer(app);
+const secureServer = createSecureServer(options, app);
 
-server.on("error", (error: any) => {
+const handleError = (error: any) => {
   if (error.syscall !== "listen") {
     throw error;
   }
@@ -28,9 +33,18 @@ server.on("error", (error: any) => {
     default:
       throw error;
   }
-});
+}
+
+server.on("error", handleError);
+
+secureServer.on("error", handleError);
 
 server.listen(process.env.PORT || 5000, () => {
   console.log(`Listening on http://localhost:${process.env.PORT || 5000}`);
 });
+
+secureServer.listen(process.env.PORT || 4433, () => {
+  console.log(`Listening on https://localhost:${process.env.PORT || 4433}`);
+});
+
 
