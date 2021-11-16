1a1dc6703b2a170aa4c4e3c8aeab5bc0329a989a
Henri De Bel <63241061+henridev@users.noreply.github.com>
2021-11-16 08:50:18 +0100

    update

--- frontend/src/service/api.ts
+++ frontend/src/service/api.ts
@@ -26,7 +26,7 @@ const logger = (response) => {
 function axiosFactory(servicename: ENDPOINTSMAP) {
   const urlpoint = servicename;
   const service = axios.create({
-    baseURL: `https://${window.location.hostname}:8433${urlpoint}`,
+    baseURL: `http://${window.location.hostname}:5000${urlpoint}`,
     withCredentials: true,
   });
 
